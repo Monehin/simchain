435bb335e2f8e8930f00373b165e7c16561cd4cd
Emmanuel Monehin <e.monehin@live.com>
2025-08-06 13:04:23 +0200

    Deploy program to devnet and update program ID

--- simchain-relayer/src/config/programId.ts
+++ simchain-relayer/src/config/programId.ts
@@ -1,3 +1,3 @@
 // simchain-relayer/src/config/programId.ts
 // Centralized program ID for build/runtime use
-export const PROGRAM_ID = 'DMaWHy1YmFNNKhyMWaTGpY76hKPdAhu4ExMHTGHU2j8r';
+export const PROGRAM_ID = 'CA8W1tSAho1u4sUXAtD7my962wsy94BDAW6cAd3YZ6kb';
